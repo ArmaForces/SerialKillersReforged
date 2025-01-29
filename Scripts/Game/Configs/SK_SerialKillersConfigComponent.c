1058038f12301083ef53eb1c80df158404da1d19
nielu <nielu92@gmail.com>
2025-01-29 12:44:47 +0100

    remove unused property

--- Scripts/Game/Configs/SK_SerialKillersConfigComponent.c
+++ Scripts/Game/Configs/SK_SerialKillersConfigComponent.c
@@ -22,9 +22,6 @@ class SK_SerialKillersConfigComponent: ScriptComponent
 	[Attribute(uiwidget: UIWidgets.ResourceAssignArray, desc: "Vehicle to spawn", params: "et", category: "PrefabList")]
 	ref array<ref ResourceName> m_pVehiclePrefabArray;
 	
-	[Attribute( defvalue: "0.3", desc: "Chance to spawn item in redfor cache")]
-	float m_fItemCacheSpawnChance;
-	
 	private static SK_SerialKillersConfigComponent s_Instance = null;
 	
 	static SK_SerialKillersConfigComponent GetInstance()
